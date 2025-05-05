9ff71242dcc65fd9145171b378c2b6a0b6c9c287
Angelo Miguel <angelo.rosa3@etec.sp.gov.br>
2025-05-05 20:56:06 -0300

    `Refactored CSS files for consistency and minor adjustments`

--- app/static/js/scripts.js
+++ app/static/js/scripts.js
@@ -1,23 +1,34 @@
-
 /* LOGIN - Função para alternar a visibilidade da senha*/
 function changePasswordVisibility() {
-  const icon = document.getElementById("eye-icon");
-  const inputPassword = document.getElementById("password");
+	const icon = document.getElementById("eye-icon");
+	const inputPassword = document.getElementById("password");
 
-  icon.classList.toggle("fa-eye-slash");
-  icon.classList.toggle("fa-eye");
+	icon.classList.toggle("fa-eye-slash");
+	icon.classList.toggle("fa-eye");
 
-  const isPassword = inputPassword.type === "password";
-  inputPassword.type = isPassword ? "text" : "password";
+	const isPassword = inputPassword.type === "password";
+	inputPassword.type = isPassword ? "text" : "password";
+}
+
+/* GERAL - Troca o display entre none e outro que desejar */
+function openAndClose(elementId, displayType) {
+	const element = document.getElementById(elementId);
+	if (element.style.display === "none" || element.style.display === "") {
+		element.style.display = displayType;
+	} else {
+		element.style.display = "none";
+	}
 }
 
 /* DEBUG liveserver jinja*/
 document.addEventListener("DOMContentLoaded", function () {
-  const jinja = document.querySelector(".jinja");
-  const jinjaAlerts = document.querySelectorAll(".login-alert");
+	const jinjaSpans = document.querySelectorAll(".jinja")
 
-  // Verifica se há algum conteúdo nas mensagens
-  if (jinja && jinjaAlerts[0].innerHTML !== "{{ message }}") {
-    jinja.style.display = "block"; // Mostra as mensagens
-  }
+	// Verifica se há algum conteúdo nas mensagens
+	jinjaSpans.forEach(jinja => {
+		if (jinja && !(jinja.innerHTML.includes("{"))) {
+			jinja.style.display = "block"; // Mostra as mensagens
+		}
+	});
+
 });
