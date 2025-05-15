5ad0709750dfe8e96cde51b9a0b4c12b564ad5dc
Angelo Miguel <angelo.rosa3@etec.sp.gov.br>
2025-05-15 15:04:19 -0300

    `Updated CSS files with minor changes and formatting adjustments`

--- app/static/js/message_script.js
+++ app/static/js/message_script.js
@@ -1,8 +1,7 @@
-/* TODO: terminar o scroll do chat */
-/* FIXME: está ficando um espaço no final */
-window.addEventListener('DOMContentLoaded', function () {
-    const msg = document.getElementById("messages");
-    if (msg) {
-      msg.scrollTop = msg.scrollHeight;
-    }
-  });
+/* Faz com que a scroll bar do chat começe para baixo "invertido" */
+window.addEventListener("DOMContentLoaded", function () {
+	const msg = document.getElementById("messages");
+	if (msg) {
+		msg.scrollTop = msg.scrollHeight;
+	}
+});
